a2ebe4b5d9c8a239366c5683dae55250d5726c94
Christian Bewernitz <coder@karfau.de>
2018-01-31 23:35:33 +0100

    improve readability

--- src/vcs/git.spec.ts
+++ src/vcs/git.spec.ts
@@ -13,14 +13,18 @@ describe('isRepoRoot', () => {
 });
 
 describe('createLocalRepo', () => {
+
+  const INITIAL_COMMIT = '9bbde535d46dff9e18e3dab0bc011eb433ced7ef';
+  const SECOND_COMMIT = '045c9c0d3a722abcbe8423533344aef56a7fea5c';
+  const CODE_INSIGHTS_GIT_FIXTURE = 'https://github.com/karfau/code-insights-git-fixture.git';
   const fixtureDir = path.join(process.cwd(), '.test', 'git-fixture');
+
   before('clean start for fixture', () => {
     fs.removeSync(fixtureDir);
   });
+
   it('should be able to clone an https repo', async () => {
-    await createLocalRepo(
-      fixtureDir, 'https://github.com/karfau/code-insights-git-fixture.git'
-    );
+    await createLocalRepo(fixtureDir, CODE_INSIGHTS_GIT_FIXTURE);
     expect(fs.existsSync(path.join(fixtureDir, '.git'))).to.be.true;
 
     describe('listRevisions', () => {
@@ -29,16 +33,17 @@ describe('createLocalRepo', () => {
       it('should list all commits with two parameters', async () => {
         let actual = await listRevisions(fixtureDir, 'master');
         expect(actual).to.not.be.empty;
+
         expect(actual.map(rev => rev.id).slice(0, 2)).to.eql([
-          '9bbde535d46dff9e18e3dab0bc011eb433ced7ef',
-          '045c9c0d3a722abcbe8423533344aef56a7fea5c'
+          INITIAL_COMMIT,
+          SECOND_COMMIT
           ])
       });
 
       it('should list one commit with lastReported being first commit', async () => {
         const all = (await listRevisions(fixtureDir, 'master'));
         expect(
-          await listRevisions(fixtureDir, 'master', '9bbde535d46dff9e18e3dab0bc011eb433ced7ef')
+          await listRevisions(fixtureDir, 'master', INITIAL_COMMIT)
         ).to.have.lengthOf(all.length-1);
       });
     });
